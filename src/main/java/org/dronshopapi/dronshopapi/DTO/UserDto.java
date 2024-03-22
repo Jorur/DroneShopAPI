06f110918bdbf04f73cebddf431d6e8c6e84f895
Teresa <a2021110982@estudiantes.upsa.edu.bo>
2024-03-22 18:01:41 -0400

    feat: DTO

---
+++ src/main/java/org/dronshopapi/dronshopapi/DTO/UserDto.java
@@ -0,0 +1,20 @@
+package org.dronshopapi.dronshopapi.DTO;
+
+import lombok.Data;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String fullName;
+    private String username;
+    private String mail;
+    private String password;
+    private char gender;
+    private LocalDate birthDate;
+    private int phoneNumber;
+    private String Address;
+}
+
