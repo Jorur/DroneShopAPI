d5d506cb7baee3d3a2fd7887a1e2b6f47c150e74
Teresa <a2021110982@estudiantes.upsa.edu.bo>
2024-03-22 15:51:38 -0400

    Initial

---
+++ src/main/java/org/dronshopapi/dronshopapi/DronShopApiApplication.java
@@ -0,0 +1,13 @@
+package org.dronshopapi.dronshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronShopApiApplication.class, args);
+    }
+
+}
