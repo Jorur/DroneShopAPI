95e6947c344c11670c7d4050da0011c78616ac3b
Jorge Urioste Hurtado <109984697+Jorur@users.noreply.github.com>
2024-03-22 17:59:53 -0400

    feat: Making the NotFoundException

---
+++ src/main/java/org/dronshopapi/dronshopapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package org.dronshopapi.dronshopapi.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ResourceNotFoundException extends RuntimeException{
+    private final String resourceName;
+    private final String fieldName;
+    private final Long fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
